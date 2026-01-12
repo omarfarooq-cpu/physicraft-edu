d20c676f421109b56f4d29c9ed05a8ac075a08e8
omarfarooq-cpu <omarfarooqf2345@gmail.com>
2026-01-12 10:05:22 +0530

    fix whatsapp opening with js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function openWhatsapp() {
-    const phoneNumber = "7092840069"; // Replace with the desired phone number
-    const message = "Hello PhysiCraft EDU , we are interested in academic collaboration"; // Replace with your message
-    const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
-    window.open(whatsappUrl, "_blank");
+    const phoneNumber = "7092840069"; 
+    const message = "Hello PhysiCraft EDU , we are interested in academic collaboration.";
+    window.open(`https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`, '_blank');
 }
