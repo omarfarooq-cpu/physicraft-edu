7556b13edcec481fc0949a4b571943cc2f2d3b64
omarfarooq-cpu <omarfarooqf2345@gmail.com>
2026-01-13 08:58:47 +0530

    pricing html updated

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ function openWhatsapp() {
     const phoneNumber = "7092840069"; 
     const message = "Hello PhysiCraft EDU , we are interested in academic collaboration.";
     window.open(`https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`, '_blank');
-}
+}   
+                        
